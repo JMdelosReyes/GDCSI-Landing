cd5a9f275c3a0b32a4b174d1125173e5149483ae
christian-martinez-andrades <christianmartinez@hotmail.es>
2020-11-08 13:15:27 +0100

    Añadido precios

---
+++ src/components/atoms/package.js
@@ -0,0 +1,99 @@
+import React from "react"
+import styled from "styled-components"
+
+const Package = ({ title, children, active }) => {
+  return (
+    <PackageWrapper className={active ? "active" : "not-active"}>
+      <div className="content">
+        <h2>{title}</h2>
+        {children}
+      </div>
+    </PackageWrapper>
+  )
+}
+
+const PackageWrapper = styled.article`
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  max-width: 350px;
+  margin-bottom: 3rem;
+  margin-left: 10em;
+  @media (min-width: 992px) {
+    width: calc(100% / 3);
+    margin-bottom: 0;
+  }
+
+  .content {
+    background: #F2BB63;
+    color: black;
+    text-align: center;
+    padding: 1.5rem;
+    border-radius: 4px;
+    box-shadow: 0px 0px 22px 7px rgba(0, 0, 0, 0.25);
+    
+    
+    .discount{
+      color: #00BE0E
+    }
+    ul {
+      padding-left: 0;
+      list-style: none;
+      margin: 2.5rem 0;
+
+      li {
+        line-height: 26px;
+        padding-left: 0.5rem;
+      }
+
+      .linethrough {
+        text-decoration: line-through;
+        opacity: 0.25;
+      }
+    }
+  }
+
+  &.not-active {
+    button {
+      background: transparent;
+      border: 1px solid #e609b5;
+    }
+  }
+
+  &.active {
+    order: 1;
+    z-index: 3;
+    
+    @media (min-width: 992px) {
+      order: 0;
+      transform: scale(1.25);
+    }
+
+    .content {
+      background: #F2BB63;
+    }
+    &:hover {
+      filter: brightness(120%);
+      box-shadow: 0px 6px 24px rgba(242, 187, 99, 0.4);
+    }
+    &::before {
+      content: "";
+      position: absolute;
+      top: -5px;
+      right: 0px;
+      bottom: -5px;
+      left: 0px;
+      background: linear-gradient(to right, #e8cb38, #d68c15);
+      z-index: -1;
+      transform: skew(3deg, 3deg);
+      border-radius: 4px;
+    }
+  }
+`
+
+export default Package
+
+
+
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,11 @@ import Subheading from "@atoms/subheading"
 import SectionHeader from "@molecules/sectionHeader"
 import SectionSetup from "@molecules/sectionSetup"
 import SectionCheckout from "@molecules/sectionCheckout"
+import { MdDone, MdClear } from "react-icons/md"
+import Packages from "@molecules/sectionPackage.js"
+import Package from "@atoms/package.js"
+import { IconContext } from "react-icons"
+import { Link } from "react-scroll"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -53,7 +58,7 @@ const IndexPage = () => {
           <Subheading>Save money</Subheading>
           <h2>You will need less waiters.</h2>
           <p>
-            Your waiters will only have to take care of serving. 
+            Your waiters will only have to take care of serving.
             Taking note of customers will be left to Self Service, saving time and money.
           </p>
           {/* <List>
@@ -75,8 +80,8 @@ const IndexPage = () => {
           <Subheading>Save time</Subheading>
           <h2>Avoid waiting.</h2>
           <p>
-          Customers will never leave tired of waiting. 
-          Shortly after sitting down at the table, the order will be directly in the kitchen.
+            Customers will never leave tired of waiting.
+            Shortly after sitting down at the table, the order will be directly in the kitchen.
           </p>
           {/* <List>
             <li>Modern web tech without the headache</li>
@@ -95,9 +100,9 @@ const IndexPage = () => {
           <Subheading>Covid-friendly</Subheading>
           <h2>Less interaccion between customers and waitters.</h2>
           <p>
-          Fewer waiters and less contact between them. 
-          In these times, Self Service is the ideal application to keep your premises safe from Covid.
-          Besides, control your capacity and the necessary measures in a simple way
+            Fewer waiters and less contact between them.
+            In these times, Self Service is the ideal application to keep your premises safe from Covid.
+            Besides, control your capacity and the necessary measures in a simple way
           </p>
           {/* <List>
             <li>Modern web tech without the headache</li>
@@ -108,6 +113,81 @@ const IndexPage = () => {
           </List> */}
         </ContentBlock>
       </ContentContainer>
+      <Packages
+        title="Our Packages"
+        para="Choose the perfect solution for you. With benefits to suit all budgets Startup can offer amazing value and expert advice"
+      >
+        <IconContext.Provider
+          value={{
+            color: "#7FFF00",
+            size: "1.2em",
+            style: { verticalAlign: "middle", marginRight: "5px" },
+          }}
+        >
+          <Package title="FREE!" active={true}>
+            <ul>
+
+              <li>
+                <MdDone />
+                30 DAYS TRIAL
+              </li>
+              <li className="linethrough">
+                <MdClear color="red" />
+                Discount
+              </li>
+
+            </ul>
+          </Package>
+          <Package title="60€/month" active={true}>
+            <ul>
+              <li>
+                <MdDone />
+                3 month
+              </li>
+              <li>
+                <MdDone />
+                10 tables
+              </li>
+              <li className="linethrough">
+                <MdClear color="red" />
+                Discount
+              </li>
+            </ul>
+          </Package>
+          <Package title="54€/month" active={true}>
+            <ul>
+              <li>
+                <MdDone />
+                6 month
+              </li>
+              <li>
+                <MdDone />
+                10 tables
+              </li>
+              <li class="discount">
+                <MdDone  />
+                10% off
+              </li>
+            </ul>
+          </Package>
+          <Package title="45€/month" active={true}>
+            <ul>
+            <li>
+                <MdDone />
+                6 month
+              </li>
+              <li>
+                <MdDone />
+                10 tables
+              </li>
+              <li class="discount" >
+                <MdDone  />
+                12.5% off
+              </li>
+            </ul>
+          </Package>
+        </IconContext.Provider>
+      </Packages>
       <SectionCheckout />
     </Layout>
   )
